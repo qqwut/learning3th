fecc22604b0ffff00dcd8255bed1bb470539cde7
wutthichai.s <wutthichai.s@entronica.co.th>
2020-01-24 18:30:06 +0700

    update template

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -7,8 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class DashboardComponent implements OnInit {
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  clickMenu(event) {
+    let nameNav = document.getElementsByClassName('side-nav');
+    nameNav[0]['style'].display = "block";
+    nameNav[0]['style'].zIndex = 3;
+
+    let nameLogo = document.getElementsByClassName('logo');
+    nameLogo[0]['style'].width = "100%";
+  }
+
 }
--- src/app/share/menu/menu.component.ts
+++ src/app/share/menu/menu.component.ts
@@ -13,80 +13,47 @@ export class MenuComponent implements OnInit {
   ngOnInit() {
     this.items = [
       {
-        label: 'เมนู',
+        label: 'HOME',
         command: (event: any) => {
         }
       },
       {
-        label: 'File',
-        icon: 'pi pi-pw pi-file',
-        items: [{
-          label: 'New',
-          icon: 'pi pi-fw pi-plus',
-          items: [
-            { label: 'User', icon: 'pi pi-fw pi-user-plus' },
-            { label: 'Filter', icon: 'pi pi-fw pi-filter' }
-          ]
-        },
-        { label: 'Open', icon: 'pi pi-fw pi-external-link' },
-        { separator: true },
-        { label: 'Quit', icon: 'pi pi-fw pi-times' }
-        ]
+        label: 'Learning',
+        // icon: 'pi pi-pw pi-file'
       },
       {
-        label: 'Edit',
-        icon: 'pi pi-fw pi-pencil',
-        items: [
-          { label: 'Delete', icon: 'pi pi-fw pi-trash' },
-          { label: 'Refresh', icon: 'pi pi-fw pi-refresh' }
-        ]
-      },
-      {
-        label: 'Help',
-        icon: 'pi pi-fw pi-question',
+        label: 'SeleniumLibrary',
+        // icon: 'pi pi-fw pi-pencil',
         items: [
           {
-            label: 'Contents',
-            icon: 'pi pi-pi pi-bars'
+            label: 'Open Browser',
+            // icon: 'pi pi-fw pi-trash'
           },
           {
-            label: 'Search',
-            icon: 'pi pi-pi pi-search',
-            items: [
-              {
-                label: 'Text',
-                items: [
-                  {
-                    label: 'Workspace'
-                  }
-                ]
-              },
-              {
-                label: 'User',
-                icon: 'pi pi-fw pi-file',
-              }
-            ]
+            label: 'Open Browser2',
+            // icon: 'pi pi-fw pi-refresh'
+          },
+          {
+            label: 'Open Browser3',
+            // icon: 'pi pi-fw pi-refresh'
           }
         ]
       },
       {
-        label: 'Actions',
-        icon: 'pi pi-fw pi-cog',
+        label: 'BuiltIn',
+        // icon: 'pi pi-fw pi-question',
         items: [
           {
-            label: 'Edit',
-            icon: 'pi pi-fw pi-pencil',
-            items: [
-              { label: 'Save', icon: 'pi pi-fw pi-save' },
-              { label: 'Update', icon: 'pi pi-fw pi-save' },
-            ]
+            label: 'Convert To String',
+            // icon: 'pi pi-pi pi-bars'
+          },
+          {
+            label: 'Run Keyword If',
+            // icon: 'pi pi-pi pi-search'
           },
           {
             label: 'Other',
-            icon: 'pi pi-fw pi-tags',
-            items: [
-              { label: 'Delete', icon: 'pi pi-fw pi-minus' }
-            ]
+            // icon: 'pi pi-pi pi-search'
           }
         ]
       }
