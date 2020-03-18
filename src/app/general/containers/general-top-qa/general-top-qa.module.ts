e6b6f88c502cdeadf86541ff28cfe7ba0df4acb5
treenate.s@entronica.co.th <treenate.s@entronica.co.th>
2020-03-18 20:00:51 +0700

    fix

--- src/app/general/containers/general-top-qa/general-top-qa.module.ts
+++ src/app/general/containers/general-top-qa/general-top-qa.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { GeneralTopQaRoutingModule } from './general-top-qa-routing.module';
 import { GeneralTopQaListComponent } from './general-top-qa-list/general-top-qa-list.component';
 import { GeneralTopQaDetailComponent } from './general-top-qa-detail/general-top-qa-detail.component';
-
+import { AngularEditorModule } from '@kolkov/angular-editor';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [GeneralTopQaListComponent, GeneralTopQaDetailComponent],
   imports: [
     CommonModule,
-    GeneralTopQaRoutingModule
+    GeneralTopQaRoutingModule,
+    AngularEditorModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class GeneralTopQaModule { }
